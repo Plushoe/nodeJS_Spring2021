cbf79d709aeba7f9781d00e6c42928312167961b
Plushoe <thomas.anastacio@gmail.com>
2021-02-09 14:05:17 +0100

    All intro exercises

---
+++ Solutions/1._Intro_Exercises/3.Objects.js
@@ -0,0 +1,46 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = {"message": "Hello, earthling! I bring peace."};
+
+// Log the message 
+
+console.log(myObj.message);
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+
+const me = {
+    name : "Thomas",
+    age : 22
+};
+
+console.log(me);
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {
+    isAllowed : true
+};
+
+// make a rule called isAllowed and let the value be true
+
+console.log(stackOverflow.isAllowed);
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = {"about" : "Just a tribute."}
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+console.log(thisSong);
+
+// --------------------------------------
+
+
