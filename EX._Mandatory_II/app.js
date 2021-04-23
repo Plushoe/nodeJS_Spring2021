b653dddfc0a8e24c2ecd4360c388a63b5ff998d4
P.Lab <thomas.anastacio@gmail.com>
2021-04-23 10:28:37 +0200

    Moving files between pcs

---
+++ EX._Mandatory_II/app.js
@@ -0,0 +1,25 @@
+//Import Express
+const express = require("express");
+const app = express();
+//Configure Express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Set folder for resources
+app.use(express.static("public"));
+
+//Import filesystem
+const fs = require("fs");
+
+//Declare "sandwiching" html files
+const top = fs.readFileSync(__dirname + "\\public\\templates\\navbar.html", "utf-8");
+const bottom = fs.readFileSync(__dirname + "\\public\\templates\\footer.html", "utf-8")
+const landingPage =  top + fs.readFileSync(__dirname + "\\public\\templates\\index.html", "utf-8") + bottom;
+
+app.get("/", (req, res) => {
+    res.send(landingPage);
+})
+
+//Declare port to listen on
+//DEV: Set up port failsafe
+app.listen(8080);
